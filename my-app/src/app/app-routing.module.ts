f98246c9c6ad7034e575576f55736c19cf405256
Omar Gallardo <omar011700@gmail.com>
2021-06-16 14:49:15 -0500

    Servicios

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductosComponent } from './productos/productos.component';
 import { ServiciosComponent } from './servicios/servicios.component';
+import {InicioSesionComponent} from './inicio-sesion/inicio-sesion.component';
 
 const routes: Routes = [
   {path: 'home',component: HomeComponent},
   {path: 'productos',component: ProductosComponent},
   {path: 'servicios',component: ServiciosComponent},  
+  {path: 'inicio_sesion',component: InicioSesionComponent},  
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
 
 ];
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ServicioService } from './servicio.service';
 import { FormsModule } from '@angular/forms';
 import { ServiciosComponent } from './servicios/servicios.component';
 import { ProductosComponent } from './productos/productos.component';
+import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { ProductosComponent } from './productos/productos.component';
     NavbarComponent,
     ServiciosComponent,
     ProductosComponent,
+    InicioSesionComponent,
   ],
   imports: [
     BrowserModule,
