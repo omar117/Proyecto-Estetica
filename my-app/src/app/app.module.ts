af5bd53e57d4c62e0a22c4674fdece0c89be2f43
Benjamin <adonai845@hotmail.com>
2021-06-18 16:22:36 -0500

    Se elimino dos archivos servicio y se agrego el servicio lector al app.module.ts

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 import { HttpClientModule } from '@angular/common/http';
-import { ServicioService } from './servicio.service';
 import { FormsModule } from '@angular/forms';
 import { ServiciosComponent } from './servicios/servicios.component';
 import { ProductosComponent } from './productos/productos.component';
 import { AyudaComponent } from './ayuda/ayuda.component';
+
+//servicio de lector
+import{ LectorService } from './lector.service';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import { AyudaComponent } from './ayuda/ayuda.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [ServicioService],
+  providers: [
+    LectorService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
