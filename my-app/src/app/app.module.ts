4fdd80935faef23d5106ae8d33675df7d1da43e8
anaeldom <anael1998dcp@gmail.com>
2021-06-19 17:12:21 -0500

    Cambios finales  - Navbar - Home - Loading -

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { ServicioService } from './servicio.service';
 import { FormsModule } from '@angular/forms';
 import { ServiciosComponent } from './servicios/servicios.component';
 import { ProductosComponent } from './productos/productos.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +22,19 @@ import { ProductosComponent } from './productos/productos.component';
     NavbarComponent,
     ServiciosComponent,
     ProductosComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [ServicioService],
   bootstrap: [AppComponent]
